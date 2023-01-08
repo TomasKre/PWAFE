179b81687dcd7b8b275aa95edd62dcb080d65e11
Tomas Krechler <tomas.krechler@tul.cz>
2023-01-08 02:31:00 +0100

    Updated login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username = new FormControl('', [Validators.required, Validators.minLength(4)]);
+  password = new FormControl(' ', [Validators.required, Validators.minLength(8)]);
+  login = new FormGroup({
+    username: this.username,
+    password: this.password
+  });
+  validationResultUsername?: string;
+  validationResultPassword?: string;
 
+  ngOnInit() {
+  }
+
+  submit() {
+    console.log(this.login);
+  }
 }
